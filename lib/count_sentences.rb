95e8bac41ac2e1fe663041faae855e212e7aaf53
Sam Ferebee <sam.ferebee@gmail.com>
2021-01-13 18:36:34 -0500

    commit tree 40c37ae0b5ad6899b34cfd2c16f9377eb6c7a442

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # total = 0
+    # total += self.count(".")
+    # total += self.count("?")
+    # total += self.count("!")
+    # total
+    total = self.split(/\?|\.|!/)
+    total.reject!{|str| str.empty?}
+    #binding.pry
+    total.size
   end
 end
